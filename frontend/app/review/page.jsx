d335420d2cee27ed4abe7e3ec8db7c93b2ffa2e3
alexjenkins-86400 <alex.jenkins@86400.com.au>
2023-06-09 17:07:35 +1000

    review page working! data displayed from backend in cards

--- frontend/app/review/page.jsx
+++ frontend/app/review/page.jsx
@@ -1,92 +1,65 @@
 "use client"
 import VotingCard from "@components/VotingCard";
 
-import { useState, useEffect} from 'react';
-// import VotingCard from "@components/VotingCard";
-// import GetToVoteOnData from "@components/GetToVoteOnData";
+import { useState, useEffect } from 'react';
 
-// import EarthCanvas from "@components/canvas/Earth";
-
-const Review = () => {
-  // const [submitting, setSubmitting] = useState(false);
-  const [response, setResponse] = useState([0,0]);
-
-  useEffect(() => {
-    // Fetch initial data
-    fetchData();
-  }, [1, 1]);
-
-  const fetchData = async () => {
-    console.log("Getting data because of use effect called.");
-    const [error, setError] = useState("");
-    // e.preventDefault();
-    setError("");
-    
-    const [key, setKey] = useState("alexisthebestchuckouttherest");
-    try {
-        const apiResponse = await fetch('http://localhost:8000/api/collect_responses', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ key }),
-        });
+const Example = () => {
+  const [cardcontent, setCardContent] = useState({ 'response': [{ 'user_input': 'Loading Data' },{'user_input': 'Please Wait...'}]});
   
-        if (apiResponse.ok) {
-          const data = await apiResponse.json();
-          DisplayVotingData(data.response)
-          console.log('Response received from backend:', data);
-          return data;
-        } else {
-          // Handle the error response
-          console.error('Request failed with status:', response.status);
-        }
-      } catch (error) {
-      console.error('An error occurred:', error);
-      return ["error", "error"];
-      }
-    };
+  const fetchData = async () => {
+    const apiResponse = await fetch('http://localhost:8000/api/collect_responses', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json',},
+      body: JSON.stringify({ "key": "alexisthebestchuckouttherest" }),
+    });
 
-  const handleThumbsUpClick = async () => {
-    console.log("Getting data because of thumbs up press.");
-    try {
-      const data = await fetchData();
-      setResponse(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      return ["error", "error"];
-    }
+    const data = await apiResponse.json();
+    setCardContent(data);
   };
-
-  return (
-    <div className='w-full flex-center flex-col 2xl:mt-10 stacked_containers'>
-      <h1 className='head_text orange_gradient text-center top_content'>
-        Would you let Earth live?
-      </h1>
-      <p className='flex-center flex-col py-0 mt-2 sm:mt-0 xl:mt-5 bottom_spacing'></p>
-        {/* <div className='flex-center flex-col mt-8 lg:mt-10 xl:mt-20 2xl:mb-24 bottom_spacing'></div> */}
-          <p className='desc text-center flex-col top_content'>
-            If you were the alien, recieving these messages<br />
-            which one would you be more likely to let live?
-          </p>
-      {/* <div className='flex-center flex-col mt-8 lg:mt-10 xl:mt-20 2xl:mb-24 bottom_spacing'></div> */}
-      <div className="container mx-auto px-4">
-        <div className="grid gap-4 md:grid-cols-2">
-          <div>
-            <VotingCard response={response[0]} onThumbsUpClick={handleThumbsUpClick}/>
-          </div>
-          <div>
-            <VotingCard response={response[1]} onThumbsUpClick={handleThumbsUpClick}/>
+  useEffect(() => {
+    if (cardcontent.response[0].user_input === 'Loading Data') {
+      fetchData();
+    }
+  }, []);
+  const renderContent = () => {
+    return (
+      <div className='w-full flex-center flex-col 2xl:mt-10 stacked_containers'>
+        <h1 className='head_text orange_gradient text-center top_content'>
+          Would you let Earth live?
+        </h1>
+        <p className='flex-center flex-col py-0 mt-2 sm:mt-0 xl:mt-5 bottom_spacing'></p>
+        <p className='desc text-center flex-col top_content'>
+          If you were the alien, which of these messages<br />
+          would convince you more to let Earth live?
+          {/* {JSON.stringify(cardcontent['response'])} */}
+          <button className='text-white black_btn' onClick={() => fetchData()}>Get Data</button>
+        </p>
+        <div className="container mx-auto px-4">
+          <div className="grid gap-4 lg:grid-cols-2 text-white">
+            <div>
+              <VotingCard content={JSON.stringify(cardcontent['response'][0]['user_input'])} fetcher = {fetchData} />
+            </div>
+            <div>
+              <VotingCard content={JSON.stringify(cardcontent['response'][1]['user_input'])} fetcher = {fetchData} />
+            </div>
           </div>
         </div>
+        <div className='w-full flex-center flex-col mt-14 xl:mt-4 2xl:mt-20 h-[500px] sm:h-[700px] xl:h-[800px] 2xl:h-[900px] behind_div'>
+        </div>
       </div>
-      <div className='w-full flex-center flex-col mt-14 xl:mt-4 2xl:mt-20 h-[500px] sm:h-[700px] xl:h-[800px] 2xl:h-[900px] behind_div'>
-        {/* <EarthCanvas /> */}
-      </div>
-  </div>
-  );
+    );
+  };
+  
+  return renderContent();
 };
 
-export default Review;
+export default Example;
 
-
+//   const handleThumbsUpClick = async () => {
+//     try {
+//       await fetchData();
+//       console.log('Response:', response);
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
--- frontend/components/VotingCard.jsx
+++ frontend/components/VotingCard.jsx
@@ -1,37 +1,26 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-const VotingCard = ({ response, onThumbsUpClick }) => {
+const VotingCard = ({ content, fetcher }) => {
+
+  console.log(content);
   const [thumbsUp, setThumbsUp] = useState(false);
 
   const handleThumbsUpClick = () => {
     setThumbsUp(true);
-    onThumbsUpClick();
+    fetcher();
   };
 
   return (
     <div className="voting_card">
-      <p className="my-4 font-satoshi text-sm text-gray-700">{response}</p>
+      
+      <p className="my-4 font-satoshi text-sm text-white">{content}</p>
 
       <div className="flex justify-center items-center mt-5">
         <button
           onClick={handleThumbsUpClick}
           className={`thumbs_up_btn ${thumbsUp ? 'thumbs_up_btn_active' : ''}`}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M5 15l7-7 7 7"
-            />
-          </svg>
         </button>
       </div>
     </div>
