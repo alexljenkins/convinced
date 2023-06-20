774a02062102f205abbacfdd76713a6a10355d07
alexjenkins-86400 <alex.jenkins@86400.com.au>
2023-06-21 00:12:34 +1000

    removed junk files

--- backend/app.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-import logging
-
-if __name__ == "__main__":
-
-    logger = logging.getLogger("uvicorn")
-
-    uvicorn.run("src.api.api:app", host="0.0.0.0", port=8000, reload=True, access_log=False)
