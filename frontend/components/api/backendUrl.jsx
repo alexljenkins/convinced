1cd6fbeeb4fd5a2c5070c67f6ce4f5bd53fac973
ajenkins <alex.jenkins@campgemini.com>
2023-08-25 00:28:37 +1000

    updated url to backend

--- frontend/components/api/backendUrl.jsx
+++ frontend/components/api/backendUrl.jsx
@@ -1,2 +1,1 @@
-export const backend_url = "http://worker.convinced.app.localspace:8000";
-// export const backend_url = "http://localhost:8000";
+export const backend_url = "http://localhost:8000";
