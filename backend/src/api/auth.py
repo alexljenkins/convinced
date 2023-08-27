e492036df1150afa8dd22957a6015e379cda9464
alexjenkins-86400 <alex.jenkins@86400.com.au>
2023-08-27 21:17:35 +1000

    hard coded key

--- backend/src/api/auth.py
+++ backend/src/api/auth.py
@@ -10,9 +10,10 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 async def get_api_key(api_key_header: str = Security(api_key_header)):
-    return True
-    if api_key_header == os.getenv('API_KEY'):
+    if api_key_header == 'alexisthebestchuckouttherest':
         return api_key_header
+    # if api_key_header == os.getenv('API_KEY'):
+    #     return api_key_header
     else:
         raise HTTPException(
             status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
